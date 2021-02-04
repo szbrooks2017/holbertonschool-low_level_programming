a373b35586ea79c1804a2a0271d75059f5316782
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-04 22:53:25 +0000

    percent d added to printf statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int n;
 srand (time(0));
 n = rand() - RAND_MAX
 if (n > 0)
-printf("is positive", n);
+printf(" % d is positive", n);
 else if (n = 0)
-printf("is zero", n);
+printf(" % d is zero", n);
 else if (n < 0)
-printf("is negative", n);
+printf(" % d is negative", n);
 return (0);
 }
