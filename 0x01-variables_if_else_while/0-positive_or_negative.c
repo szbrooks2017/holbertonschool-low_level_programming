dd79529191262cf0f1a588043232ad70bdc98b1a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-04 22:45:16 +0000

    printf command that is if then statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main- begins the statement
+ *
+ *Return: returns the int, which is 0
+ */
+
+int main(void)
+{
+int n;
+srand (time(0));
+n = rand() - RAND_MAX
+if (n > 0)
+printf("is positive", n);
+else if (n = 0)
+printf("is zero", n);
+else if (n < 0)
+printf("is negative", n);
+return (0);
+}
