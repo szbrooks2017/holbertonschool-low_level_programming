410d6b2cc8ffca4eda05c21285a16de62e942457
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-04 02:56:38 +0000

    with main and return in the comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 /**
+* main- with main the statement begins
+*
 * printf- prints to the screen
+*
+* Return- with return you return the integer
+*
 */
 
-Int main(void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,/n");
 return(0);
