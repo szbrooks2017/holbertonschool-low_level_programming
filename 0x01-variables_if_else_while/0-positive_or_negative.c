20f2ca10e3ab33b88f4c5513214a7ae3a6b87873
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-04 22:57:48 +0000

    added a new line to the printf statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,13 @@ int main(void)
 {
 int n;
 srand (time(0));
-n = rand() - RAND_MAX
+n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf(" % d is positive", n);
-else if (n = 0)
-printf(" % d is zero", n);
+printf(" % d is positive\n", n);
 else if (n < 0)
-printf(" % d is negative", n);
+printf(" % d is negative\n", n);
+printf(" % d is zero\n", n);
+else if (n = 0)
+printf(" % d is zero\n", n);
 return (0);
 }
