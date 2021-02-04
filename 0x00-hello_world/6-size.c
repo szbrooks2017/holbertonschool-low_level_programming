a839b39a0af240f36e77ef3f3e39aa22014bb63e
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-04 04:42:12 +0000

    maybe

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main- with main you can begin the statement
+*
+* Return: with return you have to give back what you took
+*/
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
