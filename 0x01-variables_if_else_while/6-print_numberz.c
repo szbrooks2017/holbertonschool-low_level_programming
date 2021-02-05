cb269e2628b3eeb20b60fc950e0eb10a1da3813c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-05 23:45:32 +0000

    for loop instead of while loop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - is the beggining
+*
+* Return: will be 0
+*/
+
+int main(void)
+{
+
+char *  a;
+for (a = 0; a < 10; a++)
+putchar (a);
+putchar ("\n");
+return (0);
+}
