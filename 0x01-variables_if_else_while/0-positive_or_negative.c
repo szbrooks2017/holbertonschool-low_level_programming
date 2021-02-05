8f291ff9e985515961c91de20c89f8dc37c9e304
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-05 00:29:55 +0000

    just spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,17 +7,20 @@
 *
 *Return: returns the int, which is 0
 */
+
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n == 0)
 printf("%d is zero\n", n);
 else if (n > 0)
 printf("%d is positive\n", n);
 else (n < 0)
 printf("%d is negativen\n", n);
+
 return (0);
 }
