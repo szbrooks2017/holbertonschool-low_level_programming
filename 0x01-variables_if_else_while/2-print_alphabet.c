36888de47d80948c1f83ff3a46db55dd291b9b24
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-05 05:06:42 +0000

    mm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main- begins the statement
+*
+*Return: returns the int, which is 0
+*/
+
+int main(void)
+{
+int a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
