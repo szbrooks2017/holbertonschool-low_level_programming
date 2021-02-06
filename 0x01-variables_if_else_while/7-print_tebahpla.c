adb7152b97537627997ff8bc115855d92f726a00
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-06 00:11:52 +0000

    needs formatting

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - begins the statement
+ *
+ * Return: is always 0
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 'z'; i >= 'a'; i--)
+putchar (i);
+putchar ("\n");
+return (0);
+}
