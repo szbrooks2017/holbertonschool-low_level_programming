f9a0272cadddf37c4032de9c782f1ee41c86abc8
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-06 01:04:39 +0000

    with continue

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for function
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+
+int a;
+for (a = 48; a < 58; i++)
+
+{
+putchar (a);
+if (a == 57)
+continue;
+putchar (44);
+putchar(32);
+}
+putchar(10);
+return (0);
+}
