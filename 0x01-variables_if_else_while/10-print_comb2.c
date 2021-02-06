3a396664a24192c36559b9cf2fd194c4b7152159
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-06 01:19:30 +0000

    maybe

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - starts the program
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+
+for(int i = 0; i < 100 ; i++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
