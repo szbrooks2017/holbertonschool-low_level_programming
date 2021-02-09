181d37fab7f80db8a99ad7a628de900f3df3de8f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-09 19:50:53 +0000

    fixed teh retun

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* _abs - finds absolute integers
+* @n: is the integer
+* Return: returns 0 or 1
+*/
+
+int _abs(int n)
+{
+
+if (n < 0)
+return (-(n));
+else
+return (n);
+}
