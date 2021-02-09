c6c81b86d26b8972b9a343e56c3140e062b973a7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-09 18:00:37 +0000

    prints correct output insteadd of double

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* print_sign - looks for a letter
+* @n: integer
+* Return: returns 1 or 0
+*/
+
+int print_sign(int n)
+
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
