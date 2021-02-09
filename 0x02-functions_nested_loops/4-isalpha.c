d697d33ff4b79daa485be32e3fbb38731adcf708
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-09 17:02:57 +0000

    correct output' :

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - searches alphabet
+ * @c: int c
+ * Return: returns 1 or 0
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+return (0);
+}
