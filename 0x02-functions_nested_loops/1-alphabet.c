5749f13e290e21e579892adbba3d2290119be9a7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-09 04:48:12 +0000

    prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - begins the function
+ *
+ * print_alphabet - prints the alphabet
+ *
+ * Return: returns 0
+ **/
+
+void print_alphabet(void)
+{
+
+char a;
+for (a = 'a'; a <= 'z'; a++)
+_putchar (a);
+_putchar ('\n');
+return;
+}
