c5610793f7bd48ebadf6e0bb0bdd08063f529b3f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-09 01:33:30 +0000

    prints Holberton one at a time

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * main - begins the function statement
- *
- * Return: returns 0
- */
+* main - begins the function statement
+*
+* Return: returns 0
+*/
 
 int main(void)
 {
-printf("Holberton\n");
+_putchar ('H');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+_putchar ('\n');
+
 return (0);
 }
