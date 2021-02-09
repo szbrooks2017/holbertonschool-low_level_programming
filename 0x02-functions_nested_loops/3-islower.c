3696bc0729c9da3d77cd969d056d815d82561672
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-09 16:43:56 +0000

    checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * islower - searches for lowercase
+ *
+ * @c: character
+ *
+ * Return: returns 1 or 0
+ **/
+
+int _islower(int c)
+{
+if (c >= 141 && c <= 171)
+return (1);
+else
+return (0);
+}
