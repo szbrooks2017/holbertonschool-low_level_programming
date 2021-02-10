d5ae02b2c42753f965acef2f3db3839c4de200e0
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-10 00:32:40 +0000

    i think i understand'

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - prints the last digit
+* @n: is the integer run through the test
+* Return: is what's returned
+*/
+
+int print_last_digit(int n)
+{
+int i = n % 10;
+if (i >= 0)
+{
+_putchar (i + '0');
+return (i);
+}
+else
+{
+_putchar ((-1 * i) + '0');
+return (i * -1);
+}
+return (0);
+}
