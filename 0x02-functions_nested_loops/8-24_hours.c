f9723f3966d92711967f00123c6927f38012b8fc
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-10 22:26:48 +0000

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* jack_bauer - whoever that is
+* Return: returns 0
+*/
+
+void jack_bauer(void)
+{
+
+int m;
+int s;
+for(m = 0; m < 24; m++)
+for(s = 0; s < 60; s++)
+{
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar(58);
+_putchar((s / 10) + '0');
+_putchar((s % 10) + '0');
+_putchar('\n');
+}
+}
