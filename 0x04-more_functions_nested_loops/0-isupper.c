c3db7d4cb40a20c260b7689e179f2c51947beea4
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-11 18:34:22 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/*
+* _isupper - looks for upper
+* @c: is an int
+* Return: returns an int
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
