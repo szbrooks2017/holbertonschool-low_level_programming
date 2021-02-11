cec3b099fe0b7b2e32e66bdb08846756fe643f5a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-11 00:06:27 +0000

    betty styled

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,8 @@ void jack_bauer(void)
 
 int m;
 int s;
-for(m = 0; m < 24; m++)
-for(s = 0; s < 60; s++)
+for (m = 0; m < 24; m++)
+for (s = 0; s < 60; s++)
 {
 _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
