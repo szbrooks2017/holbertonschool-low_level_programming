49d046ce9ccbbbf8c51a7bd53d43a98c587f78be
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-12 01:44:45 +0000

    mm

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* print_line - prints line
+* @n: is an int
+* Return: 0
+*/
+
+void print_line(int n)
+{
+int a;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+
+for (a = 0; a < n; a++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
