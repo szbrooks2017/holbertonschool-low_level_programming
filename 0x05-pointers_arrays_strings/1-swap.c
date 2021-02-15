805957d9301b7b2c6dbc4dbee1ae7f9de697f14a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-15 20:20:28 +0000

    fixed header and completed question 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* swap_int - swaps two integers
+* @a: is an int
+* @b: is an int
+* Return: returns 0
+*/
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+
+/**
+a = *a -*b;
+b = *a + *b;
+a = *b - *a;
+*/
+}
