7bfdb5e96906f3432e27088d65e2567d4b271904
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-15 19:57:19 +0000

    completed first task and checked

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - updates the value to 98
+*
+* Return: returns 0
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+N INTRO to arrays and pointers!
