0e310caccba15f119692faf87afd8d8aa50512c8
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-17 01:56:49 +0000

    correct output

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
-*
-*
+* print_array - prints the array
+* @n: is the length
+* @a: is the string
 */
 
 void print_array(int *a, int n)
 {
 int i;
-	for (i = 0; i < n; i++)
-	{
-		printf("%d, ", *(a + i));
-	}
+for (i = 0; i < n; i++)
+{
+printf("%d, ", *(a + i));
+}
 printf("\n");
 }
