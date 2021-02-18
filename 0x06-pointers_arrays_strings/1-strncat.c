12d2d27b52dfae4f3338c9b30d4496decffce382
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-18 19:20:12 +0000

    redirect dest to n instead of length

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+* *_strcat - concatenates the string
+* @dest: is the destinatin
+* @src: is the source
+* Return: returns dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+        int dest_length = _strlen(dest);
+        int appending;
+
+        for (appending = 0; appending < n && src[appending] != '\0'; appending++)
+
+        dest[dest_length + appending] = src[appending];
+        dest[dest_length + appending] = '\0';
+return (dest);
+}
+
+/**
+* _strlen - looks at the string
+* @s: is an int
+* Return: returns 0
+*/
+int _strlen(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+c++;
+return (c);
+}
