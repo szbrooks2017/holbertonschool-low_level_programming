a7a89f790b899a25d6cb4c372b4e5b68e719315e
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-18 18:14:56 +0000

    correct output

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+* *_strcat - concatenates the string
+* @dest: is the destinatin
+* @src: is the source
+* Return: returns dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_length = _strlen(dest);
+	int appending;
+
+	for (appending = 0; appending < dest_length; appending++)
+
+	dest[dest_length + appending] = src[appending];
+	dest[dest_length + appending] = '\0';
+return (dest);
+}
+
+/**
+* _strlen - looks at the string
+* @s: is an int
+* Return: returns 0
+*/
+int _strlen(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+c++;
+return (c);
+}
