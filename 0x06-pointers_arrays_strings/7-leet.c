bd895f069f9f90e8ce603484d46cdd47c37024b7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-20 02:08:58 +0000

    mm

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,6 +2,7 @@
 /**
 * *leet - leets man
 * Return: returns 0
+* @s: is a string
 */
 
 char *leet(char *s)
@@ -9,15 +10,13 @@ char *leet(char *s)
 	int j;
 	int i;
 	char let[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char numb[] = {'4', '3', '0', '7', '1'};
-/*
+	char numb[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
 	for (i = 0; s[i] != '\0'; i++)
-*/	
-	for (i = 0; s[i] != '\0'; i++)		
 	{
-		for (j = 0; let[j]; j++)
-			if (*s == let[j])
-			*s = numb[j];
+		for (j = 0; j < 10; j++)
+			if (s[i] == let[j])
+			s[i] = numb[j];
 	}
 return (s);
 }
