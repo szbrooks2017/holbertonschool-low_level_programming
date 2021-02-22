dcbcf7ec65bedcdbd06f7ae30086a3c9dcad2bc6
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-22 18:00:23 +0000

    first commit, betty compliant

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* *_memset - fills the memory with a constant byte
+* @s: is the pointer to n
+* @b: is the value
+* @n: is the length of what is to be filled
+* Return: returns a pointre to s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *mem = s, val = b;
+
+	for (i = 0; i < n; i++)
+		i[s] = val;
+
+return (mem);
+}
