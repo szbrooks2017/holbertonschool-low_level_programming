4ea4132c03f0739c3d7e84a40fff98fa39939c18
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-24 02:27:43 +0000

    betty compliant, used with a break

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,18 +6,22 @@
 * Return: returns the number of characters in s1 which only consists of s2
 */
 
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
-	int j;
+	int i, j, c = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != s[i]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-		if (accept[j] == '\0')
-		return (i);
-		}
+			if (s[i] == accept[j])
+			{
+			c++;
+			break;
+			}
+	if (accept[j] == '\0')
+	break;
 	}
-return (i);
+return (c);
 }
