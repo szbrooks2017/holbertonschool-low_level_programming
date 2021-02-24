7fe9d6ba3da8ca2db1f0b350cab132669961b329
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-24 22:07:36 +0000

    function that acts like pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - calculates power
+* @x: is the base number
+* @y: is the power number
+* Return: returns positvie and negative ints
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y != 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
