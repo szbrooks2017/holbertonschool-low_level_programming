9ab6f1b7e70530b39c7c6044a028e569c96a075a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-24 21:40:41 +0000

    betty compliant

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* factorial - factors ints
+* @n: is a number
+* Return: returns nothing
+*/
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
