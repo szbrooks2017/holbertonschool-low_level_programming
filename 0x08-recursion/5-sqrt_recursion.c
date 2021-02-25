4887f3dadff7bff20aacb6a60ee77bfdcdbdee06
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-25 01:07:18 +0000

    omg'

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+int _sqrt(int x, int y);
+/**
+* _sqrt - is the square root
+* @y: is the root
+* @x: is the base number
+* Return: returns the square root
+*/
+
+int _sqrt(int x, int y)
+{
+	int sqrt = y * y;
+
+	if (sqrt > x)
+	return (-1);
+
+	if (sqrt == x)
+	return (y);
+
+	return (_sqrt(x, y + 1));
+}
+/**
+* _sqrt_recursion - finds the square root
+* @n: is an int
+* Return: is void
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 0));
+}
