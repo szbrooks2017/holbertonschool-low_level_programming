6d466d04a0c0d03bcf4be8511a7084f10e896124
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-25 04:56:51 +0000

    header file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,3 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _pali(char *s, char *z);
+int _prime(int n, int i);
+int _sqrt(int x, int y);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,3 +13,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int _putchar(char c);
+
+#endif
