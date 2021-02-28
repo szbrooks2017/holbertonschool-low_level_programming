1bf002f148528646f246f5d95c9190f5f5bfcc70
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-28 20:24:09 +0000

    changed the name without compiling

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints name from command line
+* @argc: counts arguments
+* @argv: an array of pointers
+* Return: returns 0 for success
+*/
+
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+return (0);
+}
