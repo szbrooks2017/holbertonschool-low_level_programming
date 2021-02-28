08aa593898ed47314eaf7e13c26522e0866e2e92
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-28 20:44:04 +0000

    prints teh number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* main - prints name from command line
+* @argc: counts arguments
+* @argv: an array of pointers
+* Return: returns 0 for success
+*/
+
+int main(int argc, char **argv)
+{
+	(void) **argv;
+
+	printf("%d\n", argc - 1);
+
+return (0);
+}
