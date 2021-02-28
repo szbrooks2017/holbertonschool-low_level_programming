682a22293945780f487b2ed74c880153977aa485
szbrooks2017 <strattonbrooks13@gmail.com>
2021-02-28 22:19:44 +0000

    adds

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* main - is the main argument
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: returns 1 if false and 0 if true
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
