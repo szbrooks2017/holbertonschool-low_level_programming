9983311cf36f8fe9440afcdc0a5ec7713526fc6a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-01 01:20:54 +0000

    with another 4 loop

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,19 +10,21 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, digit;
 	int sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+		for (digit = 0; argv[i][digit]; digit++)
 		{
-			printf("Error\n");
-			return (1);
-		}
+			if (argv[i][digit] >= '0' && argv[i][digit] <= '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		sum += atoi(argv[i]);
-	}
+		}
+	}	
 	printf("%d\n", sum);
-	return (0);
-
+return (0);
 }
