88b4bd80beb4b963e916a1382b3f405a3079dcce
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-03 03:10:01 +0000

    betty compliant

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* *_strdup - duplicate a string
+* @str: the string to duplicate
+* Return: the string duplicated
+*
+*/
+
+char *_strdup(char *str)
+{
+	int len = 0, a = 0;
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	duplicate = malloc(sizeof(char) * len + 1);
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while (a < len)
+	{
+		duplicate[a] = str[a];
+
+		a++;
+	}
+	duplicate[a] = '\0';
+
+return (duplicate);
+}
