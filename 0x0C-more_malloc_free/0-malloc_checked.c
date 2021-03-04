e7223088d8da1fa349f6e3cae964c7501a52448f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-04 16:59:45 +0000

    first commit, readme, header file, first task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* *malloc_checked - allocates memory with malloc
+* @b: is an unsigned int
+* Return: returns a void pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	exit(98);
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+
+#endif
