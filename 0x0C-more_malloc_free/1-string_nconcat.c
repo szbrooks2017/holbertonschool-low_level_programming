98d565356f3bc93799d865a9055c098b16bebd6a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-05 16:41:56 +0000

    dif else statement for malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, x = n, l = 0;
+	unsigned int i = 0, j = 0, x, l = 0;
 	char *cat;
 
 	if (s1 == NULL)
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 
 	while (s2[j])
-                j++;
+		j++;
 
 		l = i + n;
 
