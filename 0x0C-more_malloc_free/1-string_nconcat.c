4d68bf1114a8a08f9c32aed3b246dc2ca94a127b
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-05 15:40:11 +0000

    dif else statement for malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i]; i++)
 
-		cat[x] = s1[i];
+		cat[x++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
 		cat[x++] = s2[i];
