83f7b8eefd4378538911358cafd91cf75aad2ebc
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-05 01:52:56 +0000

    creates a calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/*
+* _calloc - allocates mem beg with 0
+* @size: the byte size of each array element
+* @nmemb: number of elements
+*
+* Return: returns a pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, len = 0;
+	char *str;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	len = nmemb * size;
+
+	str = malloc(len);
+
+	if (str == NULL)
+		return (NULL);
+
+	while (i < len)
+	{
+		str[i] = '\0';
+		i++;
+	}
+
+return (str);
+}
