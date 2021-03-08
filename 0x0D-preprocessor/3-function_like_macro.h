d9d6dd327543456beb79ed36932e8dc27a695e21
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-08 22:00:26 +0000

    macro that finds the absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+* ABS - finds the absolute value of x
+* @x: is the parameter of the macro
+*/
+
+#define ABS(x) ((x) < 0 ? (-1) : (x))
+
+#endif
