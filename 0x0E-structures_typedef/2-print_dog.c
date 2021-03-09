b99f6e538b9c224139a403473ab1a4cfafd8276f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-09 02:27:44 +0000

    adding task two printf

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_dog - prints all the data of a dog
+* @d: a dog struct
+* Return: returns nothing
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
