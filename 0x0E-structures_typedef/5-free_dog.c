9957a894c3eea80d419b17da053a9f07ef9374ce
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-09 19:31:55 +0000

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free-dog - let the dogs out
+* @d: is the dog to be freed
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name0;
+	free(d);
+}
