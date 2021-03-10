aece31277bea437118fb4a5d80614091f05b4c57
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-10 20:55:07 +0000

    fixed quote

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int dig1, dig2, dig3;
 
-	for (dig1 = '0'; dig1 < '9'; dig1++)
+	for (dig1 = '0'; dig1 <= '9'; dig1++)
 	{
-		for (dig2 = '0'; dig2 < '9'; dig2++)
+		for (dig2 = '0'; dig2 <= '9'; dig2++)
 		{
-			for (dig3 = '0'; dig3 < '9'; dig3++)
+			for (dig3 = '0'; dig3 <= '9'; dig3++)
 			{
 				if (dig1 < dig2 && dig2 < dig3)
 				{
