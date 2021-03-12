28abec4788742ae8b0723f73c9512c9cf73834d2
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-12 06:16:59 +0000

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,10 @@ void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
+
+typedef struct form_types
+{
+	char *symbol;
+	void (*print)(va_list args);
+} format_t;
 #endif
