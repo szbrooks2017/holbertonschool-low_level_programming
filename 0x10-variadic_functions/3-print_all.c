0ffac21605a650db4448389e645aec3964c1ece3
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-12 16:21:29 +0000

    including null byte

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,7 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format[index] && format != NULL)
+	while (format[index] != '\0' && format != NULL)
 	{
 		j_dex = 0;
 		while (j_dex < 4)
