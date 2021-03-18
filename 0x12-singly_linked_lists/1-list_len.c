4a5022e497228eb9437febc54350955b9c018d5d
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-18 19:56:14 +0000

    a function that counts the length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* list_len - gives the count of the elements
+* @h: is the head or current node
+*
+* Return: returns the count
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != '\0')
+	{
+		count++;
+		h = h->next;
+	}
+return (count);
+}
