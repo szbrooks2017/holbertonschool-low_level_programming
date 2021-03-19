1c8c620b534795493c9c9986b1ea87746a46c882
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-19 03:16:11 +0000

    valgrind free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+
+}
