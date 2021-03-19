4f14b8cdb8ecb3b9411560d3d92eeab67ddd06c7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-19 03:38:50 +0000

    before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+/**
+* premain - is a premain
+*/
+#include "lists.h"
+#include <stdio.h>
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
