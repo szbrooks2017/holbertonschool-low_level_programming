240dec29a6c9f8c38e6e417654071ca995b28115
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-23 05:16:31 +0000

    switching count with head pointer

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
 * print_listint - prints the linked list
 * @h: head of the list of elements
@@ -10,12 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-
-return (count);
+	return (count);
 }
