e6229c08536cae48f62166de476e71121190a2ba
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-23 05:56:10 +0000

    adjusting loop

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		count++;
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
 	return (count);
