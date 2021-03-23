1e2d0411a3b86801fe068e747145bc405b6f1295
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-23 18:58:12 +0000

    adding if for head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 
 	listint_t *temp = *head;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
