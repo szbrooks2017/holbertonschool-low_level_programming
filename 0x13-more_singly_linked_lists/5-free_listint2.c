09fdc987b0eebeca18055365118bc1b181d44c41
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-23 19:02:42 +0000

    arranging dereferencing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,11 @@ void free_listint2(listint_t **head)
 
 	listint_t *temp = *head;
 
-	if (head == NULL)
-		return;
-
-	while (*head != NULL)
+	while (head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	head = NULL;
+	*head = NULL;
 }
