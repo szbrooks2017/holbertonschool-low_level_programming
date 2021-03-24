2d2b9bb06c3102989ac4fdf64210319384a87c55
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-24 04:02:07 +0000

    correct output

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	idx--;
 	while (idx != 1)
 	{
+		if (temp->next == NULL)
+			return (NULL);
 		temp = temp->next;
 		idx--;
 	}
