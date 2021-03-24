88d6fe05c2b5501b51f9d9acc639fe61b07bc778
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-24 02:30:42 +0000

    adding task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* sum_listint - calculates the sum of all the n of a listint
+* @head: a pointer to the head of the listint
+* Return: if the list is empty 0 otherwise the sum.
+*/
+
+int sum_listint(listint_t *head)
+{
+
+	int sum = 0;
+
+	if (!head)
+		return (0);
+
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
