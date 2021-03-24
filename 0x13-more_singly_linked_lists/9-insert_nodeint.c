0870a50483be21039de17fc2fc711aad1e3899a7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-24 04:03:54 +0000

    correct output

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *temp = *head;
 
+	if (*head == NULL)
+		return (NULL)
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
