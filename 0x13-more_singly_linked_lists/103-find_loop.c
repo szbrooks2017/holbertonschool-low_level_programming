4b7f957058da1740f7cdd0c2d76d28699933d335
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-24 04:59:27 +0000

    adding tortie and the hare

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,38 @@
+#include "list.h"
+
+/**
+* find_listint_loop - finds the loop like tortie and hare
+* @head: points to the header of a linked list
+* Return: returns the adddress where the loop starts or null
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortie, *hare;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tortie = head->next;
+	hare = head->next->next;
+
+	while (hare)
+	{
+		if (tortie == hare)
+		{
+			tortie = head;
+
+			while (tortie != hare)
+			{
+				tortie = tortie->next;
+				hare = hare->next;
+			}
+			return (tortie);
+		}
+		tortie = tortie->next;
+		hare = hare->next->next;
+	}
+	return (NULL);
+}
+
+}
