1eb786b3d1b69b66cbf9c27fa9159bd2b4430a48
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-25 23:09:48 +0000

    adding taks 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* set_bit - sets teh bit
+* @n: is th thing
+* @index: is the
+* Return: returns a thing
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
+}
