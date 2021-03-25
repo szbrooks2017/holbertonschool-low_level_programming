666f03a6f53c45d8abe811daed0b561203376728
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-25 23:18:57 +0000

    adding task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+
+#include "holberton.h"
+
+/**
+* clear_bit - resets to o
+* @n: the number to reset
+* @index: index to change
+* Return: returns 1 or -1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
