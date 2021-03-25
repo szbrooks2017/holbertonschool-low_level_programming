f4c5abefb62314213cb2ce88c0eb7e5b4f8b4816
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-25 23:41:45 +0000

    adding task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+unsigned int countSetBits(unsigned long int n);
+/**
+* countSetBits - counts the bits of a flip
+* @n: the number to reset
+* Return: returns 1 or -1
+*/
+
+unsigned int countSetBits(unsigned long int n)
+{
+	unsigned int count = 0;
+
+	while (n > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+	return (count);
+}
+/**
+* flip_bits - counts the bits of a flip
+* @n: the number to reset
+* @m: index to change
+* Return: returns 1 or -1
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (countSetBits(n ^ m));
+}
