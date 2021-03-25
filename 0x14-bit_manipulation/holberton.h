99c5cfe95c34a118b79fd42f49f23d8fb9254199
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-25 23:52:24 +0000

    adding header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLB_H
 #define HOLB_H
 
+#include <stdio.h>
+#include <stdlib.h>
+int get_endianness(void);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
