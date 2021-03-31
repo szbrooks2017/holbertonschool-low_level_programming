12102415e087ff1031148bb985af7ccfe7ed117c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-31 01:12:36 +0000

    adding task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r;
+	ssize_t o, r, w;
 	char *buff;
 
 	if (filename == NULL)
@@ -27,9 +27,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (r == -1)
 	return (0);
 
-	write(STDOUT_FILENO, buff, r);
+	w = write(STDOUT_FILENO, buff, r);
+	if (w == -1)
+	return (0);
 
 	free(buff);
 	close(o);
-	return (r);
+	return (w);
 }
