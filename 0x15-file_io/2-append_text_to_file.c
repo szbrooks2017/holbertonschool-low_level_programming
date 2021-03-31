265d0ee0dda5beb5b08c68218d28027babe1d2bf
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-31 03:42:42 +0000

    adding task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		length_bytes++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
+	o = open(filename, O_RDWR | O_APPEND);
 		if (o == -1)
 		return (-1);
 
