3c7efdb5135d1a11d2745fe381bb8907c8ac00aa
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-31 05:02:59 +0000

    MM

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
 	inputFD = open(argv[1], O_RDONLY);
 	if (inputFD == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	outputFD = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (outputFD == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
@@ -36,26 +36,26 @@ int main(int argc, char *argv[])
 	{
 		if (write(outputFD, buf, r) != r)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 		}
 	}
 
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	if (close(inputFD) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", intputFD);
 		exit(100);
 	}
 
-	if (close(inputFD) == -1)
+	if (close(outputFD) == -1)
 	{       
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", outputFD);
 		exit(100);
 	}
 return (0);
