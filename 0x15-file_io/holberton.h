2353f190938268345ba5619679061d961325dc39
szbrooks2017 <strattonbrooks13@gmail.com>
2021-03-31 01:24:06 +0000

    adding header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,8 +2,9 @@
 #define HOLBERTON_H
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/uio.h>
 #include <fcntl.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <unistd.h>
 
 /* prototypes */
