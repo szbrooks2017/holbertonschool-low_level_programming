4891921209382a78f0e326fa05f30abf70342f2b
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-02 04:08:38 +0000

    how to find island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+
+    land = 0
+
+    water = 0
+
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column] == 1:
+                land += 1
+                if row > 0 and grid[row - 1][column] == 1:
+                    water += 1
+                if column > 0 and grid[row][column - 1] == 1:
+                    water += 1
+    return land*4 - water*2
