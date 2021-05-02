b9a7455339bd36fe9b22b9f78ec7c21e715bb743
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-02 19:37:30 +0000

    adding correct def

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,8 +8,8 @@ def island_perimeter(grid):
         for column in range(len(grid[0])):
             if grid[row][column] == 1:
                 land += 1
-                if row > 0 and grid[row - 1][column] == 1:
+                if row > 0 and grid[row][column - 1] == 1:
                     water += 1
-                if column > 0 and grid[row][column - 1] == 1:
+                if column > 0 and grid[row - 1][column] == 1:
                     water += 1
     return land*4 - water*2
