ec322ac8d6138aac6a530a3f5eee7b34b18c1abd
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-03 05:54:51 +0000

    test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O ../wutlul.so dynamic lib https://github.com/szbrooks2017/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/wutlul.so
-export LD_PRELOAD=../wutlul.so
+wget -O /tmp/wutlul.so dynamic lib https://github.com/szbrooks2017/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/wutlul.so
+export LD_PRELOAD=/tmp/wutlul.so
