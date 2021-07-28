31f8fc1bc7b49a67937c4a8a63b70051be43ced1
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 20:18:14 +0000

    fixing betty

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -5,12 +5,9 @@
  * @key: is the key
  * @size: is the size of the array of the hash table
  * Return: the index at which the key/pair should be
- **/
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == NULL || size == 0)
-		return (0);
-
 	return (hash_djb2(key) % size);
 }
