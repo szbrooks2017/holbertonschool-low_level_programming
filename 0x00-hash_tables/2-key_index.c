023f7a88a17b4c1e2b00d29b1549a6e0516d68d3
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 20:21:46 +0000

    fixing betty

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
  * key_index - function that gives the index of the key
  * @key: is the key
  * @size: is the size of the array of the hash table
+ * 
  * Return: the index at which the key/pair should be
  */
 
