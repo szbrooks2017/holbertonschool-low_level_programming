ca84c447d22194498a84c0a8d47e57e811c07b9b
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 16:57:16 +0000

    bettying

--- 0x00-hash_tables/hash_tables.h
+++ 0x00-hash_tables/hash_tables.h
@@ -20,9 +20,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -33,10 +33,11 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
+
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /* prototypes */
