6e4f2678182d49c151f0dc556ea14f30f3b90837
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 22:56:51 +0000

    Adding Task 4

---
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return: returns the value at the element or null
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    /* declare a new node variable */
+    unsigned long int idx = 0;
+    hash_node_t *tmp = NULL;
+
+    /* if key == NULL, return NULL */
+    if (key == NULL)
+    {
+        return (NULL);
+    }
+
+	/* set id variable equal to the new key index using task 2 */
+	idx = key_index((unsigned char *)key, ht->size);
+
+    /* set temp to array index*/
+	tmp = ht->array[idx];
+    if (tmp == NULL)
+    {
+        return (NULL);
+    }
+
+    /* while loop: iterate through hash table starting at the beginning */
+    while (strcmp(key, tmp->key) != 0)
+    {
+        tmp = tmp->next;
+    }
+    
+    return (tmp->value);
+
+}
