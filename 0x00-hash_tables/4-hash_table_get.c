473fe5c56c0ebce4005e24978ca1bbdc4a376b3f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 23:01:52 +0000

    Fixing betty

--- 0x00-hash_tables/4-hash_table_get.c
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -10,31 +10,31 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
     /* declare a new node variable */
-    unsigned long int idx = 0;
-    hash_node_t *tmp = NULL;
+	unsigned long int idx = 0;
+	hash_node_t *tmp = NULL;
 
     /* if key == NULL, return NULL */
-    if (key == NULL)
-    {
-        return (NULL);
-    }
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
 
 	/* set id variable equal to the new key index using task 2 */
 	idx = key_index((unsigned char *)key, ht->size);
 
     /* set temp to array index*/
 	tmp = ht->array[idx];
-    if (tmp == NULL)
-    {
-        return (NULL);
-    }
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
 
     /* while loop: iterate through hash table starting at the beginning */
-    while (strcmp(key, tmp->key) != 0)
-    {
-        tmp = tmp->next;
-    }
-    
-    return (tmp->value);
+	while (strcmp(key, tmp->key) != 0)
+	{
+		tmp = tmp->next;
+	}
 
+    /* return if the comparison is true */
+	return (tmp->value);
 }
