de1d05275d7f8effdcf7d99c09c7c4160e640711
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 16:23:57 +0000

    adding betty style

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -6,31 +6,32 @@
  * @size: is the size of the array
  * Return: returns a pointer to the newly constructed table
  **/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    /* declare hash table variable */
-    hash_table_t *table = NULL;
+	/* declare hash table variable */
+	hash_table_t *table = NULL;
 
-    /* check for empty table */
-    if (size == 0)
-        return (NULL);
+	/* check for empty table */
+	if (size == 0)
+		return (NULL);
 
-    /* malloc size of table */
-    table = malloc(sizeof(hash_table_t));
+	/* malloc size of table */
+	table = malloc(sizeof(hash_table_t));
 
-    /* check for empty list */
-    if (table == NULL)
-        return (NULL);
-        
-    /* set size of our table equal to size passed */
-    table->size = size;
+	/* check for empty list */
+	if (table == NULL)
+		return (NULL);
 
-    /* malloc space for the array */
-    table->array = malloc(sizeof(hash_node_t * ) * size);
+	/* set size of our table equal to size passed */
+	table->size = size;
 
-    /* null check */
-    if (table->array == NULL)
-        return (NULL);
-    
-    return (table);
+	/* malloc space for the array */
+	table->array = malloc(sizeof(hash_node_t * ) * size);
+
+	/* null check */
+	if (table->array == NULL)
+		return (NULL);
+
+	return (table);
 }
