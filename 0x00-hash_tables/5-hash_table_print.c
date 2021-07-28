9ea6fcaf819cf6f286f636d6b51b060f6d7985ef
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 23:30:13 +0000

    adding task 5

--- 0x00-hash_tables/5-hash_table_print.c
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 
 		while (tmp != NULL)
 		{
-			printf("'%s' : '%s' ", tmp->key, tmp->value);
+			printf(" '%s' : '%s' ", tmp->key, tmp->value);
 			tmp = tmp->next;
 		}
 	}
