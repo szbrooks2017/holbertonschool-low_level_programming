0ff84cf6a49b7ee14f776234b40799395d1777f6
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 16:02:44 +0000

    Create, malloc hash table

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -6,17 +6,31 @@
  * @size: is the size of the array
  * Return: returns a pointer to the newly constructed table
  **/
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
+    /* declare hash table variable */
     hash_table_t *table = NULL;
+
+    /* check for empty table */
+    if (size == 0)
+        return (NULL);
+
     /* malloc size of table */
     table = malloc(sizeof(hash_table_t));
+
     /* check for empty list */
-    if (!table)
+    if (table == NULL)
         return (NULL);
-    /* malloc linked list */
-    array = malloc(sizeof(hash_node_t * ) * size);
-    /* make a new pointer? */
+        
+    /* set size of our table equal to size passed */
+    table->size = size;
 
+    /* malloc space for the array */
+    table->array = malloc(sizeof(hash_node_t * ) * size);
 
+    /* null check */
+    if (table->array == NULL)
+        return (NULL);
+    
+    return (table);
 }
