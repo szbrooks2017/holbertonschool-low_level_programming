99630f7cfe94004984bb25b5e11c33a430185308
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 20:25:16 +0000

    removing space

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -6,7 +6,6 @@
  * key_index - function that gives the index of the key
  * @key: is the key
  * @size: is the size of the array of the hash table
- * 
  * Return: the index at which the key/pair should be
  */
 
