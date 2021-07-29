ff62030bf1f561373fa0ed0cf70407f5a82d2f21
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-29 00:26:33 +0000

    adding task 6

--- 0x00-hash_tables/6-hash_table_delete.c
+++ 0x00-hash_tables/6-hash_table_delete.c
@@ -17,18 +17,15 @@ void hash_table_delete(hash_table_t *ht)
 
 	for (idx = 0; idx < ht->size; ++idx)
 	{
-		if (ht->array[idx])
+		while (ht->array[idx])
 		{
-			while (ht->array[idx])
-			{
-				tmp = ht->array[idx]->next;
-				free(ht->array[idx]->key);
-				free(ht->array[idx]->value);
-				free(ht->array[idx]);
-				ht->array[idx] = tmp;
-			}
+			tmp = ht->array[idx]->next;
+			free(ht->array[idx]->key);
+			free(ht->array[idx]->value);
 			free(ht->array[idx]);
+			ht->array[idx] = tmp;
 		}
+		free(ht->array[idx]);
 	}
 	free(ht->array);
 	free(ht);
