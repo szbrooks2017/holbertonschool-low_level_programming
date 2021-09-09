c828efffb1f4ca86967c24378515e9dda85b2083
szbrooks2017 <strattonbrooks13@gmail.com>
2021-09-09 21:01:33 +0000

    How to search linearly through an array

---
+++ 0x02-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches though an array for a value
+ * @array: is pointer to the first element in the array
+ * @size: is the number of elements of the array
+ * @value: is the value to be searched for
+ * Return: returns an int index wehre value is located at
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    /* loop through array */
+    size_t index;
+
+    for (index = 0; index < size; index++)
+    {
+        if (array[index] == value)
+        {
+            return index;
+        }
+        printf("Value checked array [%ld] = [%d]\n", index, array[index]);
+    }
+    return -1;
+    /* if match then print */
+}
