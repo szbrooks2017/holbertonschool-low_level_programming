32bf4eb95d9f22ec4fa9ed8f4f9f54f4a692bf12
szbrooks2017 <strattonbrooks13@gmail.com>
2021-09-10 14:48:48 +0000

    fixing print postion

--- 0x02-search_algorithms/0-linear.c
+++ 0x02-search_algorithms/0-linear.c
@@ -13,13 +13,13 @@ int linear_search(int *array, size_t size, int value)
     /* loop through array */
 	size_t index;
 
-	for (index = 0; index <= size; index++)
+	for (index = 0; index < size; index++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 		if (array[index] == value)
 		{
 			return (index);
 		}
-		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 	}
 	return (-1);
 }
